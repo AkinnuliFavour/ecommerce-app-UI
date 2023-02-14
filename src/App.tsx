c016b8175e737c290c1a0a93530295c8f390d4b4
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2023-02-14 09:35:48 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import {
+  FaArrowLeft,
+  FaSearch,
+  FaClock,
+  FaStopwatch,
+  FaArrowRight,
+  FaGift,
+  FaStar,
+  FaHouseUser,
+  FaBox,
+  FaCartPlus,
+  FaHeart,
+  FaUser,
+} from "react-icons/fa";
+import man from "./assets/man_pic.jpg";
+
+function App() {
+  return <main className="px-6 py-2"></main>;
+}
+
+export default App;
