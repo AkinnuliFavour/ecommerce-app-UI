a3a37dfefb2ce0afdab6cf3b3e0ff7c1df4b2943
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2023-03-09 15:12:50 +0100

    add routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import { useState } from "react";
-import {
-  FaArrowLeft,
-  FaSearch,
-  FaClock,
-  FaStopwatch,
-  FaArrowRight,
-  FaGift,
-  FaStar,
-  FaHouseUser,
-  FaBox,
-  FaCartPlus,
-  FaHeart,
-  FaUser,
-} from "react-icons/fa";
-import man from "./assets/man_pic.jpg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Categories from "./pages/Categories";
+import All from "./pages/categories/All";
+import Kids from "./pages/categories/Kids";
+import Men from "./pages/categories/Men";
+import Women from "./pages/categories/Women";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SingleItem from "./pages/SingleItem";
+import WishList from "./pages/WishList";
 
 function App() {
-  return <main className="px-6 py-2"></main>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/wish-list" element={<WishList />} />
+        <Route path="/single-item" element={<SingleItem />} />
+        <Route path="/mens-category" element={<Men />} />
+        <Route path="/women's-category" element={<Women />} />
+        <Route path="/kids-category" element={<Kids />} />
+        <Route path="/all" element={<All />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
